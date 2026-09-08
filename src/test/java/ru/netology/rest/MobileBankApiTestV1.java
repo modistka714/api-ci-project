7bfd3065c17035ec77bf9ea19244a0dd9a74c40d
Aleksandra Nikitina <modistka714@gmail.com>
2026-09-08 16:34:08 +0300

    Add json-schema-validator dependency to build.gradle

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -2,6 +2,7 @@ package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
+
 import static io.restassured.module.jsonschema.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
