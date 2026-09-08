d3ad7f6f7c5055cde2fa39ba7896e38f8b24e4c5
Aleksandra Nikitina <modistka714@gmail.com>
2026-09-08 19:23:16 +0300

    remove bom

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,7 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 
-import static io.restassured.module.jsonschema.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
